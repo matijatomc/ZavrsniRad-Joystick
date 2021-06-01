31fe919e65c518c6aa7133e69d47c39c81d9936e
matijatomc <55134628+matijatomc@users.noreply.github.com>
2021-06-01 12:17:25 +0200

    Add files via upload

--- joystick/joystick.ino
+++ joystick/joystick.ino
@@ -4,23 +4,29 @@
 #include <RF24.h>
 
 // Oderđivanje na kojim se pinovima nalazi CE i CSN pinovi modula
-RF24 radio(7, 8); 
+RF24 radio1(7, 8); 
+RF24 radio2(7, 8); 
 // Određivanje kanala preko kojeg će moduli komunicirat
-const byte address[6] = {"00001"}; 
+const byte address1[6] = {"00001"}; 
+const byte address2[6] = {"00002"}; 
 
 // Void koji će se odvrtiti samo jednom nakon pokretanja Arduina
 void setup() {
   Serial.begin(9600);
-  radio.begin(); // Započinjanje radio veze
+  radio1.begin(); // Započinjanje radio veze
+  radio2.begin();
   // Postavljanje adrese od kanala za odašiljač preko kojeg če komunicirat
-  radio.openWritingPipe(address); 
+  radio1.openWritingPipe(address1); 
+  radio2.openWritingPipe(address2);
   //Postavljanje maksimalne razine pojačanja snage
-  radio.setPALevel(RF24_PA_MAX); 
+  radio1.setPALevel(RF24_PA_MAX); 
+  radio2.setPALevel(RF24_PA_MAX); 
 }
 
 // Void koji će se vrtiti cijelo vrijeme dok Arduino radi
 void loop() {
-  radio.stopListening(); // Postavljanje modula kao odašiljač
+  radio1.stopListening(); // Postavljanje modula kao odašiljač
+  radio2.stopListening();
 
   // Čitanje vrijednmosti potenciometra na analognom pinu 1
   int potBrzine = analogRead(A1); 
@@ -34,9 +40,9 @@ void loop() {
 
   // Slanje vrijednost za sevo
   Serial.print("Skretanje: "); Serial.println(kutSkretanja);
-  radio.write(&kutSkretanja, sizeof(kutSkretanja)); 
+  radio1.write(&kutSkretanja, sizeof(kutSkretanja)); 
   // Slanje vrijednosti za motor
   Serial.print("Brzin: "); Serial.println(vrijednostBrzine);
-  radio.write(&vrijednostBrzine, sizeof(vrijednostBrzine)); 
+  radio2.write(&vrijednostBrzine, sizeof(vrijednostBrzine)); 
   //delay(500);
 }
